747624a0bcb0834ce9ab099e852993dbf62f633f
Peter Olsen <olsenpk@gmail.com>
2010-06-25 10:03:08 -0700

    Catch-up commit.

--- hippolyta/storage.py
+++ hippolyta/storage.py
@@ -1,98 +1,190 @@
+import os
 import mimetypes
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-from django.core.files import File
-from django.core.files.storage import Storage
-from django.utils.text import get_valid_filename
 
 try:
-    import boto
+    from cStringIO import StringIO
 except ImportError:
-    raise ImproperlyConfigured("Could not load s3boto dependency.")
+    from StringIO import StringIO
+
+from django.conf import settings
+from django.core.files.base import File
+from django.core.files.storage import Storage
+from django.core.files.base import ContentFile
+from django.utils.functional import curry
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from boto.s3.connection import S3Connection
+    from boto.s3.key import Key
+    from boto.exception import S3CreateError
+except ImportError:
+    raise ImproperlyConfigured, "Could not load Boto's S3 bindings.\
+    \nSee http://code.google.com/p/boto/"
+
+ACCESS_KEY_NAME = 'AWS_ACCESS_KEY_ID'
+SECRET_KEY_NAME = 'AWS_SECRET_ACCESS_KEY'
+HEADERS         = 'AWS_HEADERS'
+BUCKET_NAME     = 'AWS_STORAGE_BUCKET_NAME'
+DEFAULT_ACL     = 'AWS_DEFAULT_ACL'
+QUERYSTRING_AUTH = 'AWS_QUERYSTRING_AUTH'
+QUERYSTRING_EXPIRE = 'AWS_QUERYSTRING_EXPIRE'
+IS_GZIPPED         = 'AWS_IS_GZIPPED'
+LOCATION           = 'AWS_LOCATION'
+
+BUCKET_NAME     = getattr(settings, BUCKET_NAME, {})
+HEADERS           = getattr(settings, HEADERS, {})
+DEFAULT_ACL       = getattr(settings, DEFAULT_ACL, 'public-read')
+QUERYSTRING_AUTH  = getattr(settings, QUERYSTRING_AUTH, True)
+QUERYSTRING_EXPIRE= getattr(settings, QUERYSTRING_EXPIRE, 3600)
+IS_GZIPPED        = getattr(settings, IS_GZIPPED, False) 
+GZIP_CONTENT_TYPES = (
+    'text/css',
+    'application/javascript',
+    'application/x-javascript'
+)
+GZIP_CONTENT_TYPES = getattr(settings, 'GZIP_CONTENT_TYPES', GZIP_CONTENT_TYPES)
+LOCATION          = getattr(settings, LOCATION, '')
+
+if IS_GZIPPED:
+    from gzip import GzipFile
+
+class S3BotoStorage(Storage):
+    """Amazon Simple Storage Service using Boto"""
+    
+    def __init__(self, bucket_name=BUCKET_NAME, 
+            access_key=None, secret_key=None, acl=DEFAULT_ACL, headers=HEADERS,
+            gzip=IS_GZIPPED, gzip_content_types=GZIP_CONTENT_TYPES):
+
+        self.acl = acl
+        self.headers = headers
+        self.gzip = gzip
+        self.gzip_content_types = gzip_content_types
+        
+
+        if not access_key and not secret_key:
+             access_key, secret_key = self._get_access_keys()
+
+        self.connection = S3Connection(access_key, secret_key)
+        try:
+            self.bucket = self.connection.get_bucket(bucket_name, validate=True, headers=headers)
+            #self.bucket.set_acl(self.acl)
+        except S3CreateError:
+            # assuming we own it
+            self.bucket = self.connection.get_bucket(bucket_name)
+
+    def _get_access_keys(self):
+        access_key = getattr(settings, ACCESS_KEY_NAME, None)
+        secret_key = getattr(settings, SECRET_KEY_NAME, None)
+        if (access_key or secret_key) and (not access_key or not secret_key):
+            access_key = os.environ.get(ACCESS_KEY_NAME)
+            secret_key = os.environ.get(SECRET_KEY_NAME)
+
+        if access_key and secret_key:
+            # Both were provided, so use them
+            return access_key, secret_key
+
+        return None, None
+
+    def _clean_name(self, name):
+        # Useful for windows' paths
+        return os.path.normpath(name).replace('\\', '/')
+
+    def _compress_content(self, content):
+        """Gzip a given string."""
+        zbuf = StringIO()
+        zfile = GzipFile(mode='wb', compresslevel=6, fileobj=zbuf)
+        zfile.write(content.read())
+        zfile.close()
+        content.file = zbuf
+        return content
+        
+    def _open(self, name, mode='rb'):
+        name = self._clean_name(name)
+        return S3BotoStorageFile(name, mode, self)
+
+    def _save(self, name, content):
+        name = self._clean_name(name)
+        headers = self.headers
+
+        if hasattr(content.file, 'content_type'):
+            content_type = content.file.content_type
+        else:
+            content_type = mimetypes.guess_type(name)[0] or "application/x-octet-stream"
+            
+        if self.gzip and content_type in self.gzip_content_types:
+            content = self._compress_content(content)
+            headers.update({'Content-Encoding': 'gzip'})
+
+        headers.update({
+            'Content-Type': content_type,
+            'Content-Length' : len(content),
+        })
+        
+        content.name = name
+        k = self.bucket.get_key(name)
+        if not k:
+            k = self.bucket.new_key(name)
+        k.set_contents_from_file(content, headers=headers, policy=self.acl)
+        return name
+
+    def delete(self, name):
+        name = self._clean_name(name)
+        self.bucket.delete_key(name)
+
+    def exists(self, name):
+        name = self._clean_name(name)
+        k = Key(self.bucket, name)
+        return k.exists()
+
+    def listdir(self, name):
+        name = self._clean_name(name)
+        return [l.name for l in self.bucket.list() if not len(name) or l.name[:len(name)] == name]
+
+    def size(self, name):
+        name = self._clean_name(name)
+        return self.bucket.get_key(name).size
+
+    def url(self, name):
+        name = self._clean_name(name)
+        if self.bucket.get_key(name) is None:
+            return ''
+        #return self.bucket.get_key(name).generate_url(QUERYSTRING_EXPIRE, method='GET', query_auth=QUERYSTRING_AUTH)
+        return "http://%s.s3.amazonaws.com/%s" % (self.bucket.name, name)
+
+    def get_available_name(self, name):
+        """ Overwrite existing file with the same name. """
+        name = self._clean_name(name)
+        return name
 
 
-class AmazonS3Storage(Storage):
-    def __init__(self, access_key=None, secret_key=None, bucket=None):
-        """
-        Initialize the settings for the connection and container.
-        """
-        self.access_key = access_key or settings.AWS_ACCESS_KEY_ID
-        self.secret_key = secret_key or settings.AWS_SECRET_ACCESS_KEY
-        self.bucket = bucket or settings.AWS_BUCKET_NAME
-
-    def _get_connection(self):
-        if not hassattr(self, '_connection'):
-            self._connection = boto.s3.connection.S3Connection(self.access_key, self.secret_key)
-        return self._connection
-
-    def _set_connection(self, value):
-        self._connection = value
-
-    connection = property(_get_connection, _set_connection)
-
-    def _get_bucket(self):
-        if not hasattr(self, '_bucket'):
-            self.bucket = self.connection.
-
-    def _open(name, mode='rb'):
-        """
-        Return a file object.
-        """
-        pass
-
-    def _save(name, content):
-        """
-        Return the actual name of the file saved. 'content' is the file object itself.
-        """
-        pass
-
-    def get_valid_name(name):
-        """
-        Return a file name that is valid for Amazon S3.
-        """
-        pass
-
-    def get_available_name(name):
-        """
-        Return a file name that is available.
-        """
-        pass
-
-    def delete(name):
-        """
-        Remove the specified file from the Amazon S3 bucket.
-        """
-        pass
-
-    def exists(name):
-        """
-        Return true if file 'name' exists, false otherwise.
-        """
-        pass
-
-    def listdir(path):
-        """
-        Lists the contents of the specified path, returning a 2-tuple of lists;
-        the first items being directories, the second item being files.
-        """
-        pass
-
-    def size(name):
-        """
-        Returns the total size, in bytes, of the file specified by name.
-        """
-        pass
-
-    def url(name):
-        """
-        Returns an absolute URL where the file's contents can be accessed
-        directly by a web browser.
-        """
-
-
-class AmazonS3StorageFile(File):
-    def __init__(self, storage, name, *args, **kwargs):
+class S3BotoStorageFile(File):
+    def __init__(self, name, mode, storage):
         self._storage = storage
-        super(AmazonS3StorageFile, self).__init__(file=None, name=name, *args, **kwargs)
+        self.name = name
+        self._mode = mode
+        self.key = storage.bucket.get_key(name)
+        if not self.key:
+            self.key = storage.bucket.new_key(name)
+        self._is_dirty = False
+        self.file = StringIO()
 
+    @property
+    def size(self):
+        return self.key.size
 
+    def read(self, *args, **kwargs):
+        self.file = StringIO()
+        self._is_dirty = False
+        self.key.get_contents_to_file(self.file)
+        return self.file.getvalue()
+
+    def write(self, content):
+        if 'w' not in self._mode:
+            raise AttributeError("File was opened for read-only access.")
+        self.file = StringIO(content)
+        self._is_dirty = True
+
+    def close(self):
+        if self._is_dirty:
+            self.key.set_contents_from_string(self.file.getvalue(), headers=self._storage.headers, policy=self._storage.acl)
+        self.key.close()
--- hippolyta/tests.py
+++ hippolyta/tests.py
@@ -1,23 +1,29 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.core.cache import cache
+import hippolyta
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+    def setUp(self):
+        if default_storage.exists('storage_test'):
+            default_storage.delete('storage_test')
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_DoesntExist(self):
+        """Test that a file does not exist."""
+        self.assertFalse(default_storage.exists('storage_test'))
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_OpenReadFile(self):
+        file = default_storage.open('notes.txt','rb')
+        content = file.read()
+        file.close()
 
+    def test_OpenSaveFile(self):
+        self.assertFalse(default_storage.exists('storage_test.txt'))
+        file = default_storage.open('storage_test.txt','wb')
+        file.write('Now is the time for all good men to come to the aid of their country.')
+        file.close()
+    
+    def tearDown(self):
+        if default_storage.exists('storage_test.txt'):
+            default_storage.delete('storage.test.txt')
